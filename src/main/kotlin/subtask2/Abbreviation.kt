e48880110e55accef2165401be1ec54fb2905537
Aliaksanderrr <midas1989.cc@gmail.com>
2021-06-12 14:29:47 +0300

    feat: task2 completed

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -2,8 +2,28 @@ package subtask2
 
 class Abbreviation {
 
-    // TODO: Complete the following function
+
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var result: String = "YES"
+        var counterA = 0
+
+        val aList = a.toLowerCase().toList()
+        val bList = b.toLowerCase().toList()
+
+            bList.forEach {
+                if (counterA == aList.lastIndex) {
+                    result = "NO"
+                }
+                for (i in counterA..aList.lastIndex) {
+                    if (aList[counterA] == it) {
+                        counterA++
+                        return@forEach
+                    }
+                    counterA++
+                }
+                result = "NO"
+            }
+
+        return result
     }
 }
